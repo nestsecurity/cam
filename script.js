4097475c1430ba0e86f02f49c9d3a748cb089d0f
nestmanila <47880996+nestmanila@users.noreply.github.com>
2019-03-06 08:31:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,22 +66,13 @@ let fname = $("#fname").val();
 let pnumber = $("#pnumber").val();
 let eadd = $("#eadd").val();
 let pcase = $("#pcase").val();
-let sn = $("#sn").val();
 let issue = $("#issue").val();
 let in1 = $("#in1").val();
 let in2 = $("#in2").val();
-let in3 = $("#in3").val();
-let in4 = $("#in4").val();
 let ts = $("#ts").val();
-let rs = $("#rs option:selected").text();
+let config = $("#configuration").val();
 let resolution = $("#resolution").val();
 let ft = $("#ft").val();
-let device = $("#device option:selected").text();
-let devices = $("#device option:selected").val();
-let battery = $("#battery").val();
-let voc = $("#voc").val();
-let vin = $("#vin").val();
-let lin = $("#lin").val();
 let wiring = [];
 let color = [];    
    $("#submit-case").remove();
@@ -95,42 +86,15 @@ $("input[name='color']").each(function(){
        let colorarray = color.push($(this).val());
     }       
 });    
-
-if(devices == 2|| devices == 3 &&   battery === null){    
    $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
-   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n===== CONFIGURATION ===== \nDevice: " + device + "\n\nWiring and Color: \n" + wiring.join(", ") + "\n" + color.join(", ") + "\n\nPower Reading: \n" + "Battery: " + battery + "\nVOC: " + voc + "\nVIN: " + vin + "\nLIN " + lin + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
-} else if(devices == 4) {
-   $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
-   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + "\n Device: " + device + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
-} else {
-   $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
-   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
-}
-
-});
-
-$("#battery").focus(function(){
-   $(this).val("V");
-});
-
-$("#voc").focus(function(){
-   $(this).val("V");
-});
-
-$("#vin").focus(function(){
-   $(this).val("V");
-});
-
-$("#lin").focus(function(){
-   $(this).val("mA");
+   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Contact Name: " + fname + "\n Contact Phone: " + pnumber + "\n Contact E-mail: " + eadd + "\n Case Number: " + pcase + "\n\n===== CUSTOMER STATEMENT=====\n" + issue + "\n\n===== TIMELINE =====\n•Installed on: " + in1 + "\n•Issued since: " + in2 + "\n\n===== CONFIGURATION =====\n"+ config +"\n\n=====TROUBLESHOOTING =====\n" + ts + "\n\n ===== RESOLUTION =====\n" + "\n" + resolution);
 });
 
 $("#reset-btn").click(function(){ 
   $('html, body').animate({scrollTop:0}, '300');
   $('#myForm')[0].reset();
   $("#submit-case").remove();
-  $("#copy-btn").hide();
-  $("#config").hide();
+  $("#copy-btn").hide();  
 });
     
 $("#ts").focus(function() {
@@ -154,6 +118,7 @@ $("#issue").focus(function() {
         document.getElementById('issue').value +='• ';
 	}
 });
+
 $("#issue").keyup(function(event){
 	var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
@@ -163,7 +128,24 @@ $("#issue").keyup(function(event){
 	if(txtval.substr(txtval.length - 1) == '\n'){
 		document.getElementById('issue').value = txtval.substring(0,txtval.length - 1);
 	}
-});    
+});
+
+$("#configuration").focus(function() {
+    if(document.getElementById('configuration').value === ''){
+        document.getElementById('configuration').value +='• ';
+	}
+});
+
+$("#configuration").keyup(function(event){
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        document.getElementById('configuration').value +='• ';
+	}
+	var txtval = document.getElementById('configuration').value;
+	if(txtval.substr(txtval.length - 1) == '\n'){
+		document.getElementById('configuration').value = txtval.substring(0,txtval.length - 1);
+	}
+});
 
 $("#in4").focus(function() {
     if(document.getElementById('in4').value === ''){
@@ -187,76 +169,6 @@ $("#copy-btn").click(function(){
   copyText.select();
   document.execCommand("copy");
 });
-
-$(document).ready(function () {
-    $('input[name="wiring"]').on('click',function () {
-        if ($('#y1').is(':checked')) {
-          $("#input-y1").val("Yellow");  
-        } else {
-          $("#input-y1").val("");
-        } 
-        if ($('#y2').is(':checked')) {
-          $("#input-y2").val("Yellow");
-        } else {
-          $("#input-y2").val("");
-        }
-        
-        if ($('#g').is(':checked')) {
-          $("#input-g").val("Green");
-        } else {
-          $("#input-g").val("");
-        } 
-        
-        if ($('#ob').is(':checked')) {
-          $("#input-ob").val("Orange");
-        } else {
-          $("#input-ob").val("");
-        }
-        
-        if ($('#rc').is(':checked')) {
-          $("#input-rc").val("Red");
-        } else {
-          $("#input-rc").val("");
-        } 
-        
-        if ($('#w1').is(':checked')) {
-          $("#input-w1").val("White");
-        } else {
-          $("#input-w1").val("");
-        }
-        
-        if ($('#w2').is(':checked')) {
-          $("#input-w2").val("Brown");
-        } else {
-          $("#input-w2").val("");
-        }
-        
-        if ($('#g').is(':checked')) {
-          $("#input-g").val("Green");
-        } else {
-          $("#input-g").val("");
-        }
-        
-        if ($('#c').is(':checked')) {
-          $("#input-c").val("Blue");
-        } else {
-          $("#input-c").val("");
-        }
-        
-        if ($('#star').is(':checked')) {
-          $("#input-star").val("Brown");
-        } else {
-          $("#input-star").val("");
-        }
-        
-        if ($('#rh').is(':checked')) {
-          $("#input-rh").val("Red");
-        } else {
-          $("#input-rh").val("");
-        }
-    });
-});
-
           $(function(){
             $("#menu-toggle").click(function(e) {
                 e.preventDefault();
@@ -271,7 +183,7 @@ $(document).ready(function () {
               }
             });
           });
-
+          
 // intervalID to ensure only one interval runs at a tim
 let intervalID = 0;
 
