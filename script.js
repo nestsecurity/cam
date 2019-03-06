3519ed38dd31137ce2e0f4f3841b39a2a098ac91
nestmanila <47880996+nestmanila@users.noreply.github.com>
2019-03-06 09:23:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,13 +66,22 @@ let fname = $("#fname").val();
 let pnumber = $("#pnumber").val();
 let eadd = $("#eadd").val();
 let pcase = $("#pcase").val();
+let sn = $("#sn").val();
 let issue = $("#issue").val();
 let in1 = $("#in1").val();
 let in2 = $("#in2").val();
+let in3 = $("#in3").val();
+let in4 = $("#in4").val();
 let ts = $("#ts").val();
-let config = $("#configuration").val();
+let rs = $("#rs option:selected").text();
 let resolution = $("#resolution").val();
 let ft = $("#ft").val();
+let device = $("#device option:selected").text();
+let devices = $("#device option:selected").val();
+let battery = $("#battery").val();
+let voc = $("#voc").val();
+let vin = $("#vin").val();
+let lin = $("#lin").val();
 let wiring = [];
 let color = [];    
    $("#submit-case").remove();
@@ -86,8 +95,34 @@ $("input[name='color']").each(function(){
        let colorarray = color.push($(this).val());
     }       
 });    
+
+if(devices == 2|| devices == 3 &&   battery === null){    
    $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
-   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Contact Name: " + fname + "\n Contact Phone: " + pnumber + "\n Contact E-mail: " + eadd + "\n Case Number: " + pcase + "\n\n===== CUSTOMER STATEMENT=====\n" + issue + "\n\n===== TIMELINE =====\n•Installed on: " + in1 + "\n•Issued since: " + in2 + "\n\n===== CONFIGURATION =====\n"+ config +"\n\n=====TROUBLESHOOTING =====\n" + ts + "\n\n ===== RESOLUTION =====\n" + "\n" + resolution);
+   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n===== CONFIGURATION ===== \nDevice: " + device + "\n\nWiring and Color: \n" + wiring.join(", ") + "\n" + color.join(", ") + "\n\nPower Reading: \n" + "Battery: " + battery + "\nVOC: " + voc + "\nVIN: " + vin + "\nLIN " + lin + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
+} else if(devices == 4) {
+   $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
+   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + "\n Device: " + device + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
+} else {
+   $(".case").after("<textarea class='form-control mb-3' rows='30' id='submit-case'></textarea>");
+   $("#submit-case").val("\n===== CUSTOMER INFO ===== \n Full Name: " + fname + "\n Phone Number: " + pnumber + "\n E-mail Address: " + eadd + "\n Previous Case: " + pcase + "\n Serial Number: " + sn + "\n\n===== CUSTOMER STATEMENT=====\n •What's the issue? \n" + issue + "\n\n===== INVESTIGATION =====\n •When did the issue start? " + in1 + "\n•When was the Nest installed? " + in2 + "\n•Issue frequency: " + in3 + "\n•Further probing: \n" + in4 + "\n\n=====TROUBLESHOOTING =====\n Troubleshooting steps and results: \n" + ts + "\n\n ===== RESOLUTION =====\n •Can you confirm/test if it's working now? (Y/N) " + rs + "\n •What resolved the issue: " + resolution + "\n\n ===== NOTES FOR THE NEXT AGENT ===== \n• Further troubleshooting steps: " + ft);
+}
+
+});
+
+$("#battery").focus(function(){
+   $(this).val("V");
+});
+
+$("#voc").focus(function(){
+   $(this).val("V");
+});
+
+$("#vin").focus(function(){
+   $(this).val("V");
+});
+
+$("#lin").focus(function(){
+   $(this).val("mA");
 });
 
 $("#reset-btn").click(function(){ 
@@ -118,7 +153,6 @@ $("#issue").focus(function() {
         document.getElementById('issue').value +='• ';
 	}
 });
-
 $("#issue").keyup(function(event){
 	var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
@@ -128,24 +162,7 @@ $("#issue").keyup(function(event){
 	if(txtval.substr(txtval.length - 1) == '\n'){
 		document.getElementById('issue').value = txtval.substring(0,txtval.length - 1);
 	}
-});
-
-$("#configuration").focus(function() {
-    if(document.getElementById('configuration').value === ''){
-        document.getElementById('configuration').value +='• ';
-	}
-});
-
-$("#configuration").keyup(function(event){
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-        document.getElementById('configuration').value +='• ';
-	}
-	var txtval = document.getElementById('configuration').value;
-	if(txtval.substr(txtval.length - 1) == '\n'){
-		document.getElementById('configuration').value = txtval.substring(0,txtval.length - 1);
-	}
-});
+});    
 
 $("#in4").focus(function() {
     if(document.getElementById('in4').value === ''){
@@ -169,6 +186,76 @@ $("#copy-btn").click(function(){
   copyText.select();
   document.execCommand("copy");
 });
+
+$(document).ready(function () {
+    $('input[name="wiring"]').on('click',function () {
+        if ($('#y1').is(':checked')) {
+          $("#input-y1").val("Yellow");  
+        } else {
+          $("#input-y1").val("");
+        } 
+        if ($('#y2').is(':checked')) {
+          $("#input-y2").val("Yellow");
+        } else {
+          $("#input-y2").val("");
+        }
+        
+        if ($('#g').is(':checked')) {
+          $("#input-g").val("Green");
+        } else {
+          $("#input-g").val("");
+        } 
+        
+        if ($('#ob').is(':checked')) {
+          $("#input-ob").val("Orange");
+        } else {
+          $("#input-ob").val("");
+        }
+        
+        if ($('#rc').is(':checked')) {
+          $("#input-rc").val("Red");
+        } else {
+          $("#input-rc").val("");
+        } 
+        
+        if ($('#w1').is(':checked')) {
+          $("#input-w1").val("White");
+        } else {
+          $("#input-w1").val("");
+        }
+        
+        if ($('#w2').is(':checked')) {
+          $("#input-w2").val("Brown");
+        } else {
+          $("#input-w2").val("");
+        }
+        
+        if ($('#g').is(':checked')) {
+          $("#input-g").val("Green");
+        } else {
+          $("#input-g").val("");
+        }
+        
+        if ($('#c').is(':checked')) {
+          $("#input-c").val("Blue");
+        } else {
+          $("#input-c").val("");
+        }
+        
+        if ($('#star').is(':checked')) {
+          $("#input-star").val("Brown");
+        } else {
+          $("#input-star").val("");
+        }
+        
+        if ($('#rh').is(':checked')) {
+          $("#input-rh").val("Red");
+        } else {
+          $("#input-rh").val("");
+        }
+    });
+});
+
           $(function(){
             $("#menu-toggle").click(function(e) {
                 e.preventDefault();
@@ -183,110 +270,3 @@ $("#copy-btn").click(function(){
               }
             });
           });
-          
-// intervalID to ensure only one interval runs at a tim
-let intervalID = 0;
-
-// function zero-padding the integer passed as argument
-const zeroPad = num => (num < 10 ? `0${num}` : num);
-
-
-// function showing the countdown in the selected element
-// accepting a number of seconds and displaying the number of minutes and seconds
-function showTimer(seconds) {
-  // target the container in which to show the countdown and apply a class to slightly update the visual
-  const appTimer = document.querySelector('.app__timer');
-  appTimer.classList.add('active');
-
-  // compute the number of minutes and of seconds
-  const minutes = Math.floor(seconds / 60);
-  // using the modulo operator to consider the remainder past the 60s marker
-  const remainingSeconds = seconds % 60;
-
-  // add the information in the main heading
-  appTimer.querySelector('h1').textContent = `${zeroPad(minutes)}:${zeroPad(remainingSeconds)}`;
-}
-
-
-// function creating the timer
-// accepting as argument a number of seconds, setting up an interval to update the UI every second as to show the passage of time
-function timer(seconds) {
-  // immediately clear any, if existing, intervals
-  clearInterval(intervalID);
-
-  // create two dates instances, one for the moment in which the timer begins, one for the moment in which it should end
-  const now = Date.now();
-  const then = new Date(now + seconds * 1000);
-
-  // compute the number of seconds between the two SVGElementInstanceList, and immediately call the function to highlight the amount of time
-  const totalSeconds = Math.round((then - now) / 1000);
-  // immediately show the timer for the number of seconds
-  showTimer(totalSeconds);
-
-  // change the text of the sub-heading to show the time at which the timer will end
-  const appTimer = document.querySelector('.app__timer');
-  // based on the number of hours and minutes of the `then` instance
-  appTimer.querySelector('h3').textContent = `Back at ${then.getHours()}:${zeroPad(then.getMinutes())}`;
-
-  // set up the interval
-  intervalID = setInterval(() => {
-    // compute the number of seconds between the current instance and the instance represented by `then`
-    const missingSeconds = Math.round((then - Date.now()) / 1000);
-    showTimer(missingSeconds);
-
-    // when reaching 0 clear the interval and remove the arbitrary class
-    if (missingSeconds <= 0) {
-      appTimer.classList.remove('active');
-      clearInterval(intervalID);
-    }
-  }, 1000);
-  
-  $("#stop").click(function(){
-    $("#time").text("00:00");
-    $("#addtimer").text("Add timer");
-    appTimer.classList.remove('active');
-    clearInterval(intervalID);
-  });
-}
-
-
-// target the buttons which allow to run a timer for a prescribed amount of time
-const buttons = document.querySelectorAll('button');
-
-// function called in response to a click event on one of the buttons
-function handleClick() {
-  // retrieve the number of seconds described in the data.time attribute
-  // ! use the parse function to store a reference to the value as an integer
-  const seconds = parseInt(this.getAttribute('data-time'), 10);
-  // start the timer for the selected number of seconds
-  timer(seconds);
-}
-// attach an event listener on each button for a click event
-buttons.forEach(button => button.addEventListener('click', handleClick));
-
-
-// target the form detailing an integer for a selected number of minutes
-// ! it is possible to call an element by name through `document.name`
-const form = document.customTimer;
-
-// function called in response to the submit event being fired on the form
-function handleSubmit(e) {
-  // prevent the default behavior
-  e.preventDefault();
-
-  // retrieve the value detailed in the input element
-  /* minutes is the name of the input element, which means that
-      this.minutes.value
-    targets the same element as
-      this.querySelector('input[name="minutes"])
-  */
-  const minutes = parseInt(this.minutes.value, 10);
-  // start the timer for the selected number of minutes
-  timer(minutes * 60);
-
-  // clear the form of any value
-  this.reset();
-}
-
-// listen for the submit event on the entire form
-form.addEventListener('submit', handleSubmit);
